308757dfbc57b27c24cd122ab29a590263de4eb1
Mohammed Noori <kmanoorib@gmail.com>
2019-10-11 02:00:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 // Write your solution here!
-
-First, we're going to create four arrays of pets. Define 4 const called: append, prepend, removeLast and removeFirst, and set all to an initial value of ["Milo", "Otis", "Garfield"]. These are the arrays that we'll work with for each exercise.
-Append the pet "Odie" to the end of append.
-Prepend the pet "Odie" to the beginning of prepend.
-Remove the last pet from removeLast.
-Remove the first pet from removeFirst.
-
 const arr = ["Milo", "Otis", "Garfield"];
 const append = arr;
 const prepend = arr;
