901cf63e6d844a9f427dd8d4c312cd9be2cde328
Mohammed Noori <kmanoorib@gmail.com>
2019-10-11 02:06:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ append.push("Odie");
 prepend.unshift("Odie");
 removeLast.pop();
 removeFirst.shift();
+
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
