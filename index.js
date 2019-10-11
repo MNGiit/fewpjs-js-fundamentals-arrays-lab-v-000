6e3a9b0cbc0d183748ad8221b921fd9541e1a5d7
Mohammed Noori <kmanoorib@gmail.com>
2019-10-11 01:58:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your solution here!
+Append the pet "Odie" to the end of append.
+Prepend the pet "Odie" to the beginning of prepend.
+Remove the last pet from removeLast.
+Remove the first pet from removeFirst.
